7685f8e9596d88a3eda21882bdb8018ecbf5bba7
Tekken112 <quanyshbekernar7@gmail.com>
2026-04-30 15:20:15 +0500

    Add files via upload

---
+++ MyHashTable.java
@@ -0,0 +1,151 @@
+public class MyHashTable<K, V> {
+
+    private static class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public MyHashTable() {
+        chainArray = (HashNode<K, V>[]) new HashNode[M];
+        size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = (HashNode<K, V>[]) new HashNode[M];
+        size = 0;
+    }
+
+    private int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+
+        HashNode<K, V> current = chainArray[index];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                current.value = value;
+                return;
+            }
+
+            current = current.next;
+        }
+
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = chainArray[index];
+        chainArray[index] = newNode;
+
+        size++;
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+
+        HashNode<K, V> current = chainArray[index];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+
+        HashNode<K, V> current = chainArray[index];
+        HashNode<K, V> previous = null;
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                if (previous == null) {
+                    chainArray[index] = current.next;
+                } else {
+                    previous.next = current.next;
+                }
+
+                size--;
+                return current.value;
+            }
+
+            previous = current;
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    public boolean contains(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> current = chainArray[i];
+
+            while (current != null) {
+                if (current.value.equals(value)) {
+                    return true;
+                }
+
+                current = current.next;
+            }
+        }
+
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (int i = 0; i < M; i++) {
+            HashNode<K, V> current = chainArray[i];
+
+            while (current != null) {
+                if (current.value.equals(value)) {
+                    return current.key;
+                }
+
+                current = current.next;
+            }
+        }
+
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printBucketSizes() {
+        for (int i = 0; i < M; i++) {
+            int count = 0;
+
+            HashNode<K, V> current = chainArray[i];
+
+            while (current != null) {
+                count++;
+                current = current.next;
+            }
+
+            System.out.println("Bucket " + i + ": " + count);
+        }
+    }
+}
